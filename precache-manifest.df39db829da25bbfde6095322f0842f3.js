535367984ddfe8b847aadad65ad29a36639781c5
Yurii Timofeyev <georgius@ural.ru>
2019-02-08 21:50:33 +0500

    Updates

---
+++ precache-manifest.df39db829da25bbfde6095322f0842f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d7b78de03796df2fe63f0b1d247cf21",
+    "url": "/react-weather-app/static/media/Weather-PNG-File.3d7b78de.png"
+  },
+  {
+    "revision": "72a6b7ef5541b624727a",
+    "url": "/react-weather-app/static/js/runtime~main.72a6b7ef.js"
+  },
+  {
+    "revision": "a57bbdb009957da13dbf",
+    "url": "/react-weather-app/static/js/main.a57bbdb0.chunk.js"
+  },
+  {
+    "revision": "62d92c1277450b3aabd9",
+    "url": "/react-weather-app/static/js/1.62d92c12.chunk.js"
+  },
+  {
+    "revision": "a57bbdb009957da13dbf",
+    "url": "/react-weather-app/static/css/main.e3253312.chunk.css"
+  },
+  {
+    "revision": "62d92c1277450b3aabd9",
+    "url": "/react-weather-app/static/css/1.eba86eb7.chunk.css"
+  },
+  {
+    "revision": "fc406c3e66e1133690613fe5e8abf08a",
+    "url": "/react-weather-app/index.html"
+  }
+];
